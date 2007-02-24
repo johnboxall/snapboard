9f0428760a1c6b726f63d29cf3cf5d86456918de
bs1984 <bs1984@2542f447-e727-0410-bae6-81f60ff194dc>
2007-02-24 19:16:17 +0000

    - minor issue with python path
    git-svn-id: http://snapboard.googlecode.com/svn/trunk@143 2542f447-e727-0410-bae6-81f60ff194dc

--- examplesite/urls.py
+++ examplesite/urls.py
@@ -3,8 +3,9 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     # Example:
-    (r'^snapboard/', include('snapboard.urls')),
-    (r'^accounts/', include('sbreg.urls')),
+    (r'^snapboard/', include('examplesite.snapboard.urls')),
+    (r'^snapchat/', include('examplesite.snapchat.urls')),
+    (r'^accounts/', include('examplesite.sbreg.urls')),
 
     # Uncomment this for admin:
     (r'^admin/', include('django.contrib.admin.urls')),
