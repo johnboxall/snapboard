4ba8bc04bd0e744a13dce72e328440f312c4b3f5
bs1984 <bs1984@2542f447-e727-0410-bae6-81f60ff194dc>
2007-02-28 21:04:13 +0000

    - model managers module started (incomplete)
    git-svn-id: http://snapboard.googlecode.com/svn/trunk@164 2542f447-e727-0410-bae6-81f60ff194dc

---
+++ snapboard/managers.py
@@ -0,0 +1,194 @@
+# vim: ai ts=4 sts=4 et sw=4
+
+from django import newforms as forms
+from django.contrib.auth import decorators
+from django.contrib.auth import login, logout
+from django.core.paginator import ObjectPaginator, InvalidPage
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseServerError
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.utils import simplejson
+from django.views.generic.simple import redirect_to
+
+
+#from models import Thread, Post, Category, WatchList
+from models import *
+
+class PostManager(models.Manager):
+    def get_query_set(self):
+        return super(PostManager, self).get_query_set().extra(
+            select = {
+                'post_count': extra_post_count,
+                'starter': extra_starter,
+            },).order_by('-gsticky', '-date')
+
+
+# def thread(request, thread_id, page=1):
+#     try:
+#         thr = Thread.objects.get(pk=thread_id)
+#     except Thread.DoesNotExist:
+#         raise Http404
+# 
+#     render_dict = {}
+#     user = request.user
+# 
+#     if user.is_authenticated():
+#         try:
+#             wl = WatchList.objects.get(user=user, thread=thr)
+#             render_dict.update({"watched":True})
+#         except WatchList.DoesNotExist:
+#             render_dict.update({"watched":False})
+# 
+# 
+#     # this must come after the post so new messages show up
+#     uid = str(user.id)
+#     idstr = (uid + ',', ',' + uid + ',', ',' + uid)
+#     post_list = Post.objects.filter(thread=thr).order_by('odate').exclude(
+#             revision__isnull=False)
+#    
+#     # filter out the private messages.  admin cannot see private messages
+#     # (although they can use the Django admin interface to do so)
+#     # TODO: there's gotta be a better way to filter out private messages
+#     # Tested with postgresql and sqlite
+#     post_list = post_list.filter(
+#             Q(user__id__exact=user.id) |
+#             Q(private__exact='') |
+#             Q(private__endswith=idstr[2]) |
+#             Q(private__startswith=idstr[0]) |
+#             Q(private__contains=idstr[1]))
+# 
+#     # get any avatars
+#     extra_post_avatar = """
+#         SELECT avatar FROM snapboard_snapboardprofile
+#             WHERE snapboard_snapboardprofile.user_id = snapboard_post.user_id
+#         """
+#     extra_abuse_count = """
+#         SELECT COUNT(*) FROM snapboard_abusereport
+#             WHERE snapboard_post.id = snapboard_abusereport.post_id
+#         """
+#     post_list = post_list.extra( select = {
+#         'avatar': extra_post_avatar,
+#         'abuse': extra_abuse_count,
+#         })
+# 
+#     if not getattr(user, 'is_staff', False):
+#         post_list = post_list.exclude(censor=True)
+# 
+#     try:
+#         render_dict.update(paginate_context(request, Post,
+#             SNAP_PREFIX + '/threads/id/' + thread_id + '/',
+#             post_list,
+#             page,
+#             ))
+#     except InvalidPage:
+#         return HttpResponseRedirect(SNAP_PREFIX + '/threads/id/' + str(thread_id) + '/')
+# 
+#     render_dict.update({
+#             'thr': thr,
+#             'postform': postform,
+#             })
+# 
+#     return render_to_response('snapboard/thread.html',
+#             render_dict,
+#             context_instance=RequestContext(request, processors=[login_context,]))
+# 
+
+
+class ThreadManager(models.Manager):
+    def get_query_set(self):
+        '''
+        This generates a QuerySet containing Threads and additional data used
+        in generating a web page with a listing of discussions.
+        http://code.django.com/ qset allows the caller to specify an initial
+        queryset to work with.  If this is not set, all Threads will be
+        returned.
+        '''
+        # number of posts in thread
+        # censored threads don't count toward the total
+        extra_post_count = """
+            SELECT COUNT(*) FROM snapboard_post
+                WHERE snapboard_post.thread_id = snapboard_thread.id
+                AND snapboard_post.revision_id IS NULL
+                AND NOT snapboard_post.censor
+            """
+        # figure out who started the discussion
+        extra_starter = """
+            SELECT username FROM auth_user
+                WHERE auth_user.id = (SELECT user_id
+                    FROM snapboard_post WHERE snapboard_post.thread_id = snapboard_thread.id
+                    ORDER BY snapboard_post.date ASC
+                    LIMIT 1)
+            """
+        extra_last_poster = """
+            SELECT username FROM auth_user
+                WHERE auth_user.id = (SELECT user_id
+                    FROM snapboard_post WHERE snapboard_post.thread_id = snapboard_thread.id
+                    ORDER BY snapboard_post.date DESC
+                    LIMIT 1)
+            """
+        extra_last_updated = """
+            SELECT date FROM snapboard_post 
+                WHERE snapboard_post.thread_id = snapboard_thread.id
+                ORDER BY date DESC LIMIT 1
+            """
+
+        return super(ThreadManager, self).get_query_set().extra(
+            select = {
+                'post_count': extra_post_count,
+                'starter': extra_starter,
+                #'last_updated': extra_last_updated,  # bug: http://code.djangoproject.com/ticket/2210
+                # the bug is that any extra columns must match their names
+                # TODO: sorting on boolean fields is undefined in SQL theory
+                'date': extra_last_updated,
+                'last_poster': extra_last_poster,
+            },).order_by('-gsticky', '-date')
+
+
+    def get_user_query_set(self, user):
+        if SnapboardProfile.objects.filter(user=user).count() > 0:
+            profile = SnapboardProfile.objects.get(user=user)
+            if profile.frontpage_filters.all().count() > 0:
+                return self.get_query_set().filter(
+                        category__in=profile.frontpage_filters.all())
+        else:
+            return self.get_query_set()
+
+
+    def get_favorites(self, user):
+        wl = WatchList.objects.filter(user=user)
+        return self.get_query_set().filter(pk__in=[x.id for x in wl])
+
+
+    def get_private(self, user):
+        idstr = str(user.id)
+        post_list = Post.objects.filter(
+                Q(private__endswith=idstr) |
+                Q(private__startswith=idstr) |
+                Q(private__contains=idstr)).select_related()
+
+        thread_ids = [p.thread.id for p in post_list]
+        return self.get_query_set().filter(pk__in=thread_ids)
+
+
+    def get_category(self, cat_id):
+        return self.get_query_set().filter(category__id=cat_id)
+
+
+# def category_index(request):
+# 
+#     extra_post_count = """
+#         SELECT COUNT(*) FROM snapboard_thread
+#             WHERE snapboard_thread.category_id = snapboard_category.id
+#         """
+#     cat_list = Category.objects.all().extra(
+#         select = {'thread_count': extra_post_count},)
+# 
+#     return render_to_response('snapboard/category_index.html',
+#             {
+#             'cat_list': cat_list,
+#             },
+#             context_instance=RequestContext(request, processors=[login_context,]))
+
+
+# vim: ai ts=4 sts=4 et sw=4
