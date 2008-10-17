d9d1ded6556c2ce9bc193a644581ebcc697b009f
bs1984 <bs1984@2542f447-e727-0410-bae6-81f60ff194dc>
2008-10-17 20:01:01 +0000

    Removed sbreg application.
    There are many actively maintained registration apps for Django.
    
    
    git-svn-id: http://snapboard.googlecode.com/svn/trunk@186 2542f447-e727-0410-bae6-81f60ff194dc

--- sbreg/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-from views import register, confirm
-
-urlpatterns = patterns('',
-    (r'^register/$', register),
-    (r'^confirm/(?P<activation_key>[^/]+)/$', confirm),
-)
