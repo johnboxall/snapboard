302c2a0ab129422c4dc16db931177ce9fa89a70e
John Boxall <john@handimobility.ca>
2009-09-15 17:24:23 -0700

    DRYup RPC class using JsonResponse.

--- snapboard/rpc.py
+++ snapboard/rpc.py
@@ -11,6 +11,18 @@ from snapboard.templatetags.extras import render_filter
 def _sanitize(text):
     return render_filter(striptags(text), "safe")
 
+def _toggle_boolean_field(obj, field):
+    '''
+    Switches the a boolean value and returns the new value.
+    object should be a Django Model
+    '''
+    setattr(obj, field, (not getattr(obj, field)))
+    object.save()
+    return getattr(obj, field)
+    
+def JsonResponse(obj):
+    return HttpResponse(simplejson.dumps(obj), mimetype='application/javascript')
+
 
 def rpc_post(request):
     show_id = int(request.GET['show'])
@@ -26,38 +38,22 @@ def rpc_post(request):
     if post.previous is not None:
         prev_id = str(post.previous.id)
 
-    resp = {'text': _sanitize(post.text),
-            'prev_id': prev_id,
-            'rev_id': rev_id,
-            }
-    return HttpResponse(simplejson.dumps(resp), mimetype='application/javascript')
-
+    return JsonResponse({
+        'text': _sanitize(post.text),
+        'prev_id': prev_id,
+        'rev_id': rev_id,
+    })
 
 def rpc_preview(request):
-    text = request.raw_post_data 
-    return HttpResponse(simplejson.dumps({'preview': _sanitize(text)}),
-            mimetype='application/javascript')
-
+    return JsonResponse({'preview': _sanitize(request.raw_post_data )})
 
 def rpc_lookup(request, queryset, field, limit=5):
     # XXX We should probably restrict member (or other) lookups to registered users
     obj_list = []
-    lookup = { '%s__icontains' % field: request.GET['query'],}
+    lookup = {'%s__icontains' % field: request.GET['query']}
     for obj in queryset.filter(**lookup)[:limit]:
-                obj_list.append({"id": obj.id, "name": getattr(obj, field)}) 
-    object = {"ResultSet": { "total": str(limit), "Result": obj_list } }
-    return HttpResponse(simplejson.dumps(object), mimetype='application/javascript')
-
-
-def _toggle_boolean_field(object, field):
-    '''
-    Switches the a boolean value and returns the new value.
-    object should be a Django Model
-    '''
-    setattr(object, field, (not getattr(object, field)))
-    object.save()
-    return getattr(object, field)
-
+        obj_list.append({"id": obj.id, "name": getattr(obj, field)}) 
+    return JsonResponse({"ResultSet": { "total": str(limit), "Result": obj_list } })
 
 def rpc_csticky(request, **kwargs):
     if not request.user.is_staff:
@@ -67,7 +63,6 @@ def rpc_csticky(request, **kwargs):
     else:
         return {'link':_('set csticky'), 'msg':_('Removed thread from category sticky list')}
 
-
 def rpc_gsticky(request, **kwargs):
     if not request.user.is_staff:
         raise PermissionDenied
@@ -75,7 +70,6 @@ def rpc_gsticky(request, **kwargs):
         return {'link':_('unset gsticky'), 'msg':_('This thread is now globally sticky.')}
     else:
         return {'link':_('set gsticky'), 'msg':_('Removed thread from global sticky list')}
-
 
 def rpc_close(request, **kwargs):
     if not request.user.is_staff:
@@ -85,34 +79,26 @@ def rpc_close(request, **kwargs):
     else:
         return {'link':_('close thread'), 'msg':_('This discussion is now OPEN.')}
 
-
 def rpc_watch(request, **kwargs):
     thr = kwargs['thread']
     if not thr.category.can_read(request.user):
         raise PermissionError
     try:
         # it exists, stop watching it
-        wl = WatchList.objects.get(user=request.user, thread=thr)
-        wl.delete()
+        WatchList.objects.get(user=request.user, thread=thr).delete()
         return {'link':_('watch'),
                 'msg':_('This thread has been removed from your favorites.')}
     except WatchList.DoesNotExist:
         # create it
-        wl = WatchList(user=request.user, thread=thr)
-        wl.save()
+        WatchList.objects.create(user=request.user, thread=thr)
         return {'link':_('dont watch'),
                 'msg':_('This thread has been added to your favorites.')}
 
-
 def rpc_abuse(request, **kwargs):
     # TODO: test this
-    abuse = AbuseReport.objects.get_or_create(
-            submitter = request.user,
-            post = kwargs['post'],
-            )
+    AbuseReport.objects.get_or_create(submitter=request.user, post=kwargs['post'])
     return {'link': '',
             'msg':_('The moderators have been notified of possible abuse')}
-
 
 def rpc_censor(request, **kwargs):
     if not request.user.is_staff:
@@ -123,11 +109,9 @@ def rpc_censor(request, **kwargs):
         return {'link':_('censor'), 'msg':_('This post is no longer censored.')}
 
 def rpc_quote(request, **kwargs):
-    post = Post.objects.select_related().get(id=kwargs['oid'])
+    post = Post.objects.select_related().get(pk=kwargs['oid'])
     if not post.thread.category.can_read(request.user):
         raise PermissionError
     if post.is_private and post.user != request.user and not post.private.filter(id=request.user.id).count():
         raise PermissionDenied
     return {'text': post.text, 'author': unicode(post.user)}
-
-# vim: ai ts=4 sts=4 et sw=4
