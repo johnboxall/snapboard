5137d85d734d3bdc3facc0dcc258884450fdbe46
John Boxall <john@handimobility.ca>
2009-09-15 17:25:29 -0700

    ...

--- snapboard/views.py
+++ snapboard/views.py
@@ -1,19 +1,15 @@
 import logging
 import datetime
 
-from django import forms
 from django.conf import settings
-from django.contrib.auth import decorators
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
-from django.db import connection
-from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseServerError
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.utils import simplejson
 from django.utils.translation import ugettext as _
+from django.views.decorators.http import require_POST
 
 try:
     from notification import models as notification
@@ -33,19 +29,19 @@ USE_SNAPBOARD_SIGNIN = getattr(settings, 'USE_SNAPBOARD_SIGNIN', False)
 USE_SNAPBOARD_LOGIN_FORM = getattr(settings, 'USE_SNAPBOARD_LOGIN_FORM', False)
 
 RPC_OBJECT_MAP = {
-        "thread": Thread,
-        "post": Post,
-        }
+    "thread": Thread,
+    "post": Post,
+}
 
 RPC_ACTION_MAP = {
-        "censor": rpc_censor,
-        "gsticky": rpc_gsticky,
-        "csticky": rpc_csticky,
-        "close": rpc_close,
-        "abuse": rpc_abuse,
-        "watch": rpc_watch,
-        "quote": rpc_quote,
-        }
+    "censor": rpc_censor,
+    "gsticky": rpc_gsticky,
+    "csticky": rpc_csticky,
+    "close": rpc_close,
+    "abuse": rpc_abuse,
+    "watch": rpc_watch,
+    "quote": rpc_quote,
+}
         
 def render(template_name, context, request=None):
     context_instance = RequestContext(request, processors=extra_processors)
@@ -59,12 +55,19 @@ def snapboard_default_context(request):
     This should be added to the settings variable TEMPLATE_CONTEXT_PROCESSORS
     """
     return {
-            'SNAP_MEDIA_PREFIX': SNAP_MEDIA_PREFIX,
-            'SNAP_POST_FILTER': SNAP_POST_FILTER,
-            'LOGIN_URL': settings.LOGIN_URL,
-            'LOGOUT_URL': settings.LOGOUT_URL,
-            }
+        'SNAP_MEDIA_PREFIX': SNAP_MEDIA_PREFIX,
+        'SNAP_POST_FILTER': SNAP_POST_FILTER,
+        'LOGIN_URL': settings.LOGIN_URL,
+        'LOGOUT_URL': settings.LOGOUT_URL,
+    }
 
+def get_user_settings(user):
+    if not user.is_authenticated():
+        return DEFAULT_USER_SETTINGS
+    try:
+        return user.sb_usersettings
+    except UserSettings.DoesNotExist:
+        return DEFAULT_USER_SETTINGS
 
 def user_settings_context(request):
     return {'user_settings': get_user_settings(request.user)}
@@ -87,19 +90,18 @@ if USE_SNAPBOARD_LOGIN_FORM:
 else:
     extra_processors = [user_settings_context]
 
-def rpc(request):
+def rpc(request, mimetype='application/javascript'):
     """
     Delegates simple rpc requests.
     """
     if not request.POST or not request.user.is_authenticated():
         return HttpResponseServerError()
 
-    response_dict = {}
+    ctx = {}
 
-    try:
-        action = request.POST['action'].lower()
-        rpc_func = RPC_ACTION_MAP[action]
-    except KeyError:
+    action = request.POST.get('action', '').lower()
+    rpc_func = RPC_ACTION_MAP.get(action)
+    if rpc_func is None:
         raise HttpResponseServerError()
 
     if action == 'quote':
@@ -108,27 +110,25 @@ def rpc(request):
         except (KeyError, ValueError):
             return HttpResponseServerError()
 
-    try:
-        # oclass_str will be used as a keyword in a function call, so it must
-        # be a string, not a unicode object (changed since Django went
-        # unicode). Thanks to Peter Sheats for catching this.
-        oclass_str =  str(request.POST['oclass'].lower())
-        oclass = RPC_OBJECT_MAP[oclass_str]
-    except KeyError:
+    # oclass_str will be used as a keyword in a function call, so it must
+    # be a string, not a unicode object (changed since Django went
+    # unicode). Thanks to Peter Sheats for catching this.
+    oclass_str = str(request.POST.get('oclass', '').lower())
+    oclass = RPC_OBJECT_MAP.get(oclass_str)
+    if oclass is None:
         return HttpResponseServerError()
 
     try:
         oid = int(request.POST['oid'])
-
         forum_object = oclass.objects.get(pk=oid)
-
-        response_dict.update(rpc_func(request, **{oclass_str:forum_object}))
-        return HttpResponse(simplejson.dumps(response_dict), mimetype='application/javascript')
-
-    except oclass.DoesNotExist:
-        return HttpResponseServerError()
     except KeyError:
         return HttpResponseServerError()
+    except oclass.DoesNotExist:
+        return HttpResponseServerError()
+    else:
+        ctx.update(rpc_func(request, **{oclass_str: forum_object}))
+        return HttpResponse(simplejson.dumps(ctx), mimetype=mimetype)
+
 
 def thread(request, cslug, tslug, template="snapboard/thread.html"):
     thr = get_object_or_404(Thread.view_manager.filter(category__slug=cslug), slug=tslug)
@@ -156,49 +156,25 @@ def thread(request, cslug, tslug, template="snapboard/thread.html"):
     if get_user_settings(request.user).reverse_posts:
         post_list = post_list.order_by('-odate')
     
-    ctx.update({
-        'posts': post_list,
-        'thr': thr,
-        'postform': postform,
-    })
+    ctx.update({'posts': post_list, 'thr': thr, 'postform': postform})
     return render(template, ctx, request)
 
-def edit_post(request, original, next=None):
+@require_POST
+def edit_post(request, post_id):
     """
     Edit an existing post.decorators in python
     """
-    if not request.method == 'POST':
-        raise Http404
-
-    try:
-        orig_post = Post.view_manager.get(pk=int(original))
-    except Post.DoesNotExist:
-        raise Http404
-        
+    orig_post = get_object_or_404(Post.view_manager, pk=post_id)
     if orig_post.user != request.user or not orig_post.thread.category.can_post(request.user):
         raise PermissionError
-
-    postform = PostForm(request.POST)
+    
+    postform = PostForm(request.POST, request=request)
     if postform.is_valid():
-        # create the post
-        post = Post(
-                user = request.user,
-                thread = orig_post.thread,
-                text = postform.cleaned_data['post'],
-                previous = orig_post,
-                )
-        post.save()
-        post.private = orig_post.private.all()
-        post.is_private = orig_post.is_private
-        post.save()
-
-        orig_post.revision = post
-        orig_post.save()
-
+        post = postform.edit(orig_post)
         div_id_num = post.id
     else:
         div_id_num = orig_post.id
-
+    
     try:
         next = request.POST['next'].split('#')[0] + '#snap_post' + str(div_id_num)
     except KeyError:
@@ -228,7 +204,7 @@ def favorite_index(request, template="snapboard/thread_index.html"):
     by the user.
     """
     qs = Thread.view_manager.get_favorites(request.user)
-    threads = filter(lambda t: t.category.can_view(request.user), qs)
+    threads = [thr for thr in qs if thr.category.can_view(request.user)]
     ctx = {'title': _("Watched Discussions"), 'threads': threads}
     return render(template, ctx, request)
 
@@ -260,7 +236,7 @@ def thread_index(request, template="snapboard/thread_index.html"):
         qs = Thread.view_manager.get_user_query_set(request.user)
     else:
         qs = Thread.view_manager.get_query_set()
-    threads = filter(lambda t: t.category.can_view(request.user), qs)
+    threads = [thr for thr in qs if thr.category.can_view(request.user)]
     ctx = {'title': _("Recent Discussions"), 'threads': threads}
     return render(template, ctx, request)
 
@@ -289,14 +265,14 @@ def locate_post(request, post_id):
     else:
         page = preceding_count // ppp + 1
     
-    path = reverse('snapboard_thread', args=[post.thread.category.slug, post.thread.slug])
+    args = [post.thread.category.slug, post.thread.slug]
+    path = reverse('snapboard_thread', args=args)
     next = '%s?page=%i#snap_post%i' % (path, page, post.id)
     return HttpResponseRedirect(next)
 
 def category_index(request, template="snapboard/category_index.html"):
-    ctx = {
-        'cat_list': [c for c in Category.objects.all() if c.can_view(request.user)],
-    }
+    qs = Category.objects.all()
+    ctx = {'cat_list': [c for c in qs if c.can_view(request.user)]}
     return render(template, ctx, request)
 
 @login_required
@@ -305,11 +281,18 @@ def edit_settings(request, template="snapboard/edit_settings.html"):
     Allow user to edit his/her profile. Requires login.
     """
     userdata, _ = UserSettings.objects.get_or_create(user=request.user)
-    form = UserSettingsForm(request.POST or None, instance=userdata, request=request)
+    data = request.POST or None
+    form = UserSettingsForm(data, instance=userdata, request=request)
     if form.is_valid():
         form.save(commit=True)
+        return HttpResponseRedirect("")
     return render(template, {"form": form}, request)
 
+
+#@@@ Haven't looked at group functions.
+
+#@@@
+@login_required
 def manage_group(request, group_id):
     group = get_object_or_404(Group, pk=group_id)
     if not group.has_admin(request.user):
@@ -327,8 +310,9 @@ def manage_group(request, group_id):
             'snapboard/manage_group.html',
             render_dict,
             context_instance=RequestContext(request, processors=extra_processors))
-manage_group = login_required(manage_group)
 
+#@@@
+@login_required
 def invite_user_to_group(request, group_id):
     group = get_object_or_404(Group, pk=group_id)
     if not group.has_admin(request.user):
@@ -354,8 +338,9 @@ def invite_user_to_group(request, group_id):
     return render_to_response('snapboard/invite_user.html',
             {'form': form, 'group': group},
             context_instance=RequestContext(request, processors=extra_processors))
-invite_user_to_group = login_required(invite_user_to_group)
 
+#@@@
+@login_required
 def remove_user_from_group(request, group_id):
     group = get_object_or_404(Group, pk=group_id)
     if not group.has_admin(request.user):
@@ -385,8 +370,9 @@ def remove_user_from_group(request, group_id):
     else:
         raise Http404
     return HttpResponse('ok')
-remove_user_from_group = login_required(remove_user_from_group)
 
+#@@@
+@login_required
 def grant_group_admin_rights(request, group_id):
     """
     Although the Group model allows non-members to be admins, this view won't 
@@ -416,8 +402,9 @@ def grant_group_admin_rights(request, group_id):
     else:
         raise Http404
     return HttpResponse('ok')
-grant_group_admin_rights = login_required(grant_group_admin_rights)
 
+#@@@
+@login_required
 def discard_invitation(request, invitation_id):
     if not request.method == 'POST':
         raise Http404
@@ -431,8 +418,9 @@ def discard_invitation(request, invitation_id):
     else:
         request.user.message_set.create(message=_('The invitation was discarded.'))
     return HttpResponse('ok')
-discard_invitation = login_required(discard_invitation)
 
+#@@@
+@login_required
 def answer_invitation(request, invitation_id):
     invitation = get_object_or_404(Invitation, pk=invitation_id)
     if request.user != invitation.sent_to:
@@ -462,15 +450,6 @@ def answer_invitation(request, invitation_id):
     return render_to_response('snapboard/invitation.html',
             {'form': form, 'invitation': invitation},
             context_instance=RequestContext(request, processors=extra_processors))
-answer_invitation = login_required(answer_invitation)
-
-def get_user_settings(user):
-    if not user.is_authenticated():
-        return DEFAULT_USER_SETTINGS
-    try:
-        return user.sb_usersettings
-    except UserSettings.DoesNotExist:
-        return DEFAULT_USER_SETTINGS
 
 def _brand_view(func):
     """
@@ -498,5 +477,3 @@ _brand_view(remove_user_from_group)
 _brand_view(grant_group_admin_rights)
 _brand_view(discard_invitation)
 _brand_view(answer_invitation)
-
-# vim: ai ts=4 sts=4 et sw=4
