3452f7f28bb38d736deca08cb6a7c7fa811651ad
John Boxall <john@handimobility.ca>
2009-10-30 14:08:56 -0700

    ...

--- snapboard/middleware/threadlocals.py
+++ snapboard/middleware/threadlocals.py
@@ -20,4 +20,3 @@ class ThreadLocals(object):
     def process_request(self, request):
         _thread_locals.user = getattr(request, 'user', None)
         _thread_locals.ip = request.META.get('REMOTE_ADDR', None)
-# vim: ai ts=4 sts=4 et sw=4
